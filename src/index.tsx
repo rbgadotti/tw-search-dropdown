6f8dfcced59d7e9fff869a70ed6007fb38b496a7
Rafael Gadotti <rbgadotti@gmail.com>
2023-04-20 11:55:34 -0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import 'style.css'
 
 export { default as Container } from './Container'
 export { default as Input } from './Input'
-export { default as ResultCategoryProps } from './ResultCategoryProps'
-export { default as ResultContainerProps } from './ResultContainerProps'
+export { default as ResultCategory } from './ResultCategory'
+export { default as ResultContainer } from './ResultContainer'
 export { default as ResultItem } from './ResultItem'
