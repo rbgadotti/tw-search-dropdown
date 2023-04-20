19cac5c13d278d6b5c5cdc341307089594b4f1de
Rafael Gadotti <rbgadotti@gmail.com>
2023-04-20 12:30:53 -0300

    add eslint rule for empty interface

--- src/ResultItem.types.ts
+++ src/ResultItem.types.ts
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ResultItemCommonProps {}
 
 export type ResultItemAsLink = ResultItemCommonProps & {
